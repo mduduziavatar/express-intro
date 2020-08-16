5a6f2ab043257dbd2b077becbcb16c56c42cbcfa
mduduziavatar <mduduziavatar@gmail.com>
2020-08-16 20:55:42 +0200

    saving progress

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const settingsBill = SettingsFactoryFunction();
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.use(express.static('public'))
-    // parse application/x-www-form-urlencoded
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
     res.render('index', {
-
+        settings: settingsBill.getSettings(),
+        totals: settingsBill.settingsBillTotals()
     });
 });
 
@@ -27,17 +28,15 @@ app.post("/settings", function(req, res) {
         warningLevel: req.body.warningLevel,
         criticalLevel: req.body.criticalLevel
     });
-    console.log(settingsBill.getSettings())
-        // note that data can be sent to the template
-    res.render('index', {
-        settings: settingsBill.getSettings()
-    });
-
+    console.log(settingsBill.getSettings());
+    // note that data can be sent to the template
+    res.redirect("/");
 
 });
 
 app.post("/action", function(req, res) {
-
+    settingsBill.addFunction(req.body.billItemTypeWithSettings)
+    res.redirect("/");
 });
 
 app.get("/actions", function(req, res) {
